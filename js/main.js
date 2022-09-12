0c2f82a9fc9837885a0cb7934746f0b17b061ed4
rebals <rls.blue09@gmail.com>
2022-09-11 21:03:52 -0500

    Changes to certification section

--- js/main.js
+++ js/main.js
@@ -2,10 +2,17 @@
 var modal = document.getElementById("myModal");
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("myImg");
+var img01 = document.getElementById("myImg01");
+var img02 = document.getElementById("myImg02");
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
-img.onclick = function(){
+img01.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+img02.onclick = function(){
     modal.style.display = "block";
     modalImg.src = this.src;
     captionText.innerHTML = this.alt;
