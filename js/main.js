51bbc9b10a2c7b52738ed1baa45d0e85515d6b37
rebals <rls.blue09@gmail.com>
2022-09-20 21:01:25 -0500

    Update nav menu

--- js/main.js
+++ js/main.js
@@ -26,16 +26,6 @@ span.onclick = function() {
     modal.style.display = "none";
 }
 
-// Mobile menu //
-
-function myFunction() {
-    var x = document.getElementsByClassName("nav-menu");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
 
 // Scroll Back To Top Button//
 // Get the button:
