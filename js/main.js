c2083652614f7215d70f4e4357ef7e4345b43358
rebals <rls.blue09@gmail.com>
2022-09-22 21:53:44 -0500

    Update project page

--- js/main.js
+++ js/main.js
@@ -17,33 +17,3 @@ img02.onclick = function(){
     modalImg.src = this.src;
     captionText.innerHTML = this.alt;
 }
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-
-// Scroll Back To Top Button//
-// Get the button:
-let mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
